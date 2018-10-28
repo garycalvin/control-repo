8e53c6ca3a05370489e3ae90e7aae22b79bb9c9e
garycalvin <gary.calvin@gmail.com>
2018-10-28 09:42:33 -0400

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -2,11 +2,11 @@ class profile::r10k {
   class {'r10k':
     remote => 'https://github.com/garycalvin/control-repo',
   }
-  class ('r10k::webhook::config':
+  class {'r10k::webhook::config':
     use_mcollective => false,
     enable_ssl      => false,
   }
-  class ('r10k::webhook':
+  class {'r10k::webhook':
     user  => 'root',
     group => 'root',
   }
